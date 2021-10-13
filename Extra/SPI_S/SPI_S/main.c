106c3a2a20f2802587f6e2533bd67397c68c944e
Alessandro <giuffra.alessandro@gmail.com>
2021-10-13 13:37:24 -0500

    SPI maestro done - Alessandro

---
+++ Extra/SPI_S/SPI_S/main.c
@@ -0,0 +1,18 @@
+/*
+ * SPI_S.c
+ *
+ * Created: 13/10/2021 13:31:49
+ * Author : Alessandro
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
